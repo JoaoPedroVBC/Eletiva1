b100f9c7118c8b2eaa035c488d5f27fc574568f0
JoaoPedroVBC <elaine.v.v2@gmail.com>
2026-08-13 19:58:51 -0300

    Inicio do PHP

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width= , initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Ano: 2025 </p>
+    <p>Amo: <?php  echo date("Y"):;?> </p>
+</body>
+</html>
