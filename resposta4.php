cdc8d3ac626add24c6dc39d01684ecd259f65fcc
JoaoPedroVBC <elaine.v.v2@gmail.com>
2026-08-13 21:35:49 -0300

    Correção Exercicio 4

--- resposta4.php
+++ resposta4.php
@@ -1,7 +1,7 @@
 <?php 
     $valor1 = $_POST["valor1"];
     $valor2 = $_POST["valor2"];
-    $valor3 = $_POST["valoe3"];
+    $valor3 = $_POST["valor3"];
     $soma = $valor1 + $valor2 + $valor3;
     $media = $soma/3;
     echo "O VALOR É: $soma E A MEDIA É: $media";
