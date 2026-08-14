014d5741611997e38045573972705b0fc6bd3038
JoaoPedroVBC <elaine.v.v2@gmail.com>
2026-08-13 21:08:02 -0300

    1° Exercicio

---
+++ exercicio.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Exercicio de Exemplo </h1>
+    <form method="post" action="resposta.php">
+        <label for="valor1"> Informe o primeiro valor </label>
+        <input type="number" id="valor1" name="valor1"> 
+
+        <label for="valor2"> informe o valor 2 </label>
+        <input type="numer" id="valor2" name="valor2">
+
+        <button type="submit"> Enviar </button>
+    </form>
+</body>
+</html>
