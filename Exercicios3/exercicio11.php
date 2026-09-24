97c7794b2e8db75258865c1cc613fff485d5c3bd
JoaoPedroVBC <elaine.v.v2@gmail.com>
2026-09-24 14:39:14 -0300

    Exercicio 11 concluido (começo do 12)

---
+++ Exercicios3/exercicio11.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Exercicio 11</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+<h1>Exercicio 11</h1>
+<form method="post">
+<div class="mb-3">
+             <h3> <label for="valor" class="form-label">Insira um numero: </label> </h3>
+              <input type="number" id="valor" name="valor" class="form-control" required="" step="any">
+            </div>
+<div class="mb-3"> 
+            <label> <h4> 
+                <?php if ($_POST) {
+
+                    $valor = $_POST["valor"];
+
+                    echo "R$ ". number_format($valor,2,",",".");
+                }  
+                
+                
+                
+                
+                ?>
+
+
+            </h4> </label>
+
+</div>
+
+
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
